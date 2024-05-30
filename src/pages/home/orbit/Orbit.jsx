38added9d18f5a44f3ebcd372c0991ced7a2805d
Abhinaba <abhi.roychoudhury.8@gmail.com>
2024-05-30 20:57:09 +0530

    orbit done

--- src/pages/home/orbit/Orbit.jsx
+++ src/pages/home/orbit/Orbit.jsx
@@ -25,9 +25,24 @@ const Orbit = () => {
           <div className="python"></div>
         </div>
         {/* //////////////////////////////////////////////// */}
-        <div className="applyBtn">
-          <CommonButton text={"Apply Now"} paddingHV={"14px 130px"} fontSize={"14px"} fontWight={"600"}/>
+
+        <a href="https://authentication.acciojob.com/sign-up" target="_blank">
+          <div className="applyBtn">
+            <CommonButton
+              text={"Apply Now"}
+              paddingHV={"14px 130px"}
+              fontSize={"14px"}
+              fontWight={"600"}
+            />
+          </div>
+        </a>
+        <div className="applyText">
+          By clicking ‘Apply Now For Free’, you agree to our
         </div>
+        <a href="https://terms.acciojob.com/" target="_blank">
+          <div className="termText">Terms & Conditions</div>
+        </a>
+
         <div className="backgroundShadow">
           <div className="color"></div>
           <div className="color"></div>
