7fd669fa93497710c5e5e3829ec953037c7614a1
Abhinaba <abhi.roychoudhury.8@gmail.com>
2024-06-21 02:20:16 +0530

    accordian done

---
+++ src/components/accordian/Accordion.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import "./styles.scss";
+import { IoIosArrowDown } from "react-icons/io";
+
+const Accordion = ({ data, openOthers }) => {
+  const [accordionItems, setAccordionItems] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      setAccordionItems([
+        ...data.slice(0, 1).map((items) => ({
+          ...items,
+          toggled: true,
+        })),
+        ...data.slice(1).map((items) => ({
+          ...items,
+          toggled: false,
+        })),
+      ]);
+    }
+  }, [data]);
+
+  const handleAcccordionClick = (clickedItem) => {
+    setAccordionItems([
+      ...accordionItems.map((item) => {
+        let toggled = item.toggled;
+
+        if (clickedItem.id === item.id) {
+          toggled = !item.toggled;
+        } else if (!openOthers) {
+          toggled = false;
+        }
+
+        return {
+          ...item,
+          toggled,
+        };
+      }),
+    ]);
+  };
+
+  return (
+    <div className="accordianContainer">
+      {accordionItems?.map((listItems, key) => (
+        <div
+          className={`accordian ${listItems.toggled ? "toggled" : ""}`}
+          key={key}
+        >
+          <button
+            className="toggle"
+            onClick={() => handleAcccordionClick(listItems)}
+          >
+            <p>{listItems.que}</p>
+            <div
+              className="openClose"
+              style={{
+                transform: `${listItems.toggled ? "rotate(180deg)" : ""}`,
+              }}
+            >
+              <IoIosArrowDown />
+            </div>
+          </button>
+          <div className="ansContainer">
+            <div className="ans">{listItems.ans}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
